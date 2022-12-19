b70507f4e1c4401a2b428ccabedacc9208b20e91
eve rybody <janoujan@laposte.net>
2022-12-19 09:03:51 +0100

    add auth and multer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use('/api/stuff', stuffRoutes)
 app.use('/api/auth', userRoutes)
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
-const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
 
-router.post('/', stuffCtrl.createThing);
-router.get('/:id',stuffCtrl.getOneThing)
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
-router.get('/', stuffCtrl.getAllThing);
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router
